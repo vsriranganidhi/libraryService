86669c2dfc20138f714724be98e8f4f9a9ffd10c
V SRI RANGA NIDHI <vsriranganidhi7283@gmail.com>
2026-01-26 02:47:20 +0530

    Refactor database connection to use DATABASE_URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        url: process.env.DATABASE_URL,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // As we discussed, keep this false for safety!
       }),
